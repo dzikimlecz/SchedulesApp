0e3269c5ba5a6576c7e88bae79efb01ff1f07ecb
dzikimlecz <j.pul312@gmail.com>
2022-02-15 11:18:03 +0100

    implemented viewing layout

--- common/src/commonMain/kotlin/me/dzikimlecz/common/App.kt
+++ common/src/commonMain/kotlin/me/dzikimlecz/common/App.kt
@@ -1,20 +1,95 @@
 package me.dzikimlecz.common
 
-import androidx.compose.material.Text
-import androidx.compose.material.Button
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import me.dzikimlecz.common.model.Activity
+import me.dzikimlecz.common.model.Priority
+import me.dzikimlecz.common.model.Schedule
+import me.dzikimlecz.common.model.ViewMode
+import me.dzikimlecz.common.util.now
+import me.dzikimlecz.common.util.toFriendlyString
+import me.dzikimlecz.common.util.today
+import kotlin.random.Random
 
 @Composable
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+    var openedSchedule by remember { mutableStateOf<Schedule?>(null) }
+    var viewMode by remember { mutableStateOf(ViewMode.View) }
 
-    Button(onClick = {
-        text = "Hello, ${getPlatformName()}"
-    }) {
-        Text(text)
-    }
+    Scaffold(
+        content = {
+            if (openedSchedule == null) SchedulesList(getSchedulesList(3)) { openedSchedule = it }
+            else ScheduleView(getSchedulesList(1)[0])
+        },
+        topBar = {
+            TopAppBar(
+                title = {
+                    if (openedSchedule == null) Text("Schedules", Modifier.fillMaxWidth())
+                    else {
+                        val schedule = openedSchedule!!
+                        Row {
+                            Text(schedule.name, Modifier.weight(2f))
+                            Text(schedule.date.toFriendlyString(), Modifier.weight(1f))
+                        }
+                    }
+                },
+                navigationIcon = {
+                    if (openedSchedule == null)
+                        Box(Modifier.defaultMinSize(64.dp, 36.dp), Alignment.Center) {
+                            Icon(Icons.Default.DateRange, "")
+                        }
+                    else Button(onClick = { openedSchedule = null }) {
+                        Icon(Icons.Default.ArrowBack, "Back to Menu")
+                    }
+                },
+            )
+        },
+        floatingActionButton = {
+                FloatingActionButton(onClick = {}) {
+                    if (openedSchedule == null) Icon(Icons.Default.Add, "Create a new schedule")
+                    else Icon(Icons.Default.Edit, "Edit Schedule")
+                }
+        }
+    )
+}
+
+internal fun getSchedulesList(size: Int): List<Schedule> {
+    var c = 0
+    val sequence = generateSequence { Schedule("Plan ${c++}", today(), getSomeActivities()) }.iterator()
+    val list = mutableListOf<Schedule>()
+    for (i in 0 until size)
+        list += sequence.next()
+    return list
+}
+
+internal fun getSomeActivities(): List<Activity> {
+    val chars = "6l38GJgT7ifgZlg".toList()
+    var c = 0
+    val rand = Random(4654456)
+    val sequence = generateSequence { Activity(
+        "Coś ${c++}",
+        now(),
+        when(rand.nextInt(3)) {
+            0 -> Priority.Low
+            1 -> Priority.Medium
+            2 -> Priority.High
+            else -> throw AssertionError() },
+        if (rand.nextBoolean()) chars.shuffled().joinToString(separator = "") else null
+    ) }.iterator()
+    val list = mutableListOf<Activity>()
+    for (i in 0..15)
+        list += sequence.next()
+    return list
 }
