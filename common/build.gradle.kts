c0021baeba23ae73187f8a06e915c051f4064043
dzikimlecz <j.pul312@gmail.com>
2022-02-15 11:15:15 +0100

    added kotlinx datetime dependency

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -22,6 +22,7 @@ kotlin {
                 api(compose.runtime)
                 api(compose.foundation)
                 api(compose.material)
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.3.2")
             }
         }
         val commonTest by getting {
